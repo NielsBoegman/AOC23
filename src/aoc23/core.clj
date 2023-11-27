63b1ad295bb41c4642ac4f0a0ee85e592bb6af6a
pokemaster69 <n.boegman.nb@gmail.com>
2023-11-27 12:37:23 +0100

    ree

--- src/aoc23/core.clj
+++ src/aoc23/core.clj
@@ -3,4 +3,13 @@
 (defn foo
   "I don't do a whole lot."
   [x]
-  (println x "Hello, World!"))
+  (println x "Hello, World!")
+  (println "Second hello!"))
+
+(defn factorial [n]
+  (if (= n 0)
+    1
+    (* n (factorial (dec n)))))
+
+(defn printfive [_]
+  (println "5"))
